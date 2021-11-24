6fbd20b7f955216be4edccfef6c8d1fa1afd8c76
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-11-24 10:32:17 -0500

    Segundo Commit

--- src/main/java/com/nttdata/controller/ProductController.java
+++ src/main/java/com/nttdata/controller/ProductController.java
@@ -4,11 +4,9 @@ import java.net.URI;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
