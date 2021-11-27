abfb122c6ecef13694d93dac456c957987c288d8
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-11-26 23:37:01 -0500

    Segundo Commit

--- src/main/java/com/nttdata/ProductServiceApplication.java
+++ src/main/java/com/nttdata/ProductServiceApplication.java
@@ -2,11 +2,12 @@ package com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**Se crea la clase principal ProductServiceApplication.*/
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ProductServiceApplication {
 
   public static void main(String[] args) {
