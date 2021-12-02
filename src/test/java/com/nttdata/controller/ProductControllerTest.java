c11bfe961b4f0ade9cf16442e42b0e2f96accc8b
Carlos Alberto Palacios Aguado <cpalacag@emeal.nttdata.com>
2021-12-02 14:09:10 -0500

    Segundo Commit

--- src/test/java/com/nttdata/controller/ProductControllerTest.java
+++ src/test/java/com/nttdata/controller/ProductControllerTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
@@ -15,11 +16,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.nttdata.model.Condition;
 import com.nttdata.model.Product;
 import com.nttdata.repository.ProductRepository;
 import com.nttdata.service.impl.ProductServiceImpl;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Import(ProductServiceImpl.class)
 @ExtendWith(SpringExtension.class)
@@ -38,27 +41,34 @@ class ProductControllerTest {
     Product products = new Product();
     
     List<String> type = new ArrayList<>();    
-    type.add("PERSONAL");    
+    type.add("PERSONAL");
     
     products.setId("1");
     products.setProductName("AHORRO");
-    products.setProductType("PASIVO");    
-    products.getCondition().setCustomerTypeTarget(type);
-    products.getCondition().setHasMaintenanceFee(false);
-    products.getCondition().setMaintenanceFee(0.0);
-    products.getCondition().setHasMonthlyTransactionLimit(true);
-    products.getCondition().setMonthlyTransactionLimit(3);
-    products.getCondition().setHasDailyWithdrawalTransactionLimit(false);
-    products.getCondition().setDailyWithdrawalTransactionLimit(0);
-    products.getCondition().setHasDailyDepositTransactionLimit(false);
-    products.getCondition().setDailyDepositTransactionLimit(0);
-    products.getCondition().setProductPerPersonLimit(1);
-    products.getCondition().setProductPerBusinessLimit(0);
+    products.setProductType("PASIVO");
+    
+    Condition condition = new Condition();
+    
+    condition.setCustomerTypeTarget(type);
+    condition.setHasMaintenanceFee(false);
+    condition.setMaintenanceFee(0.0);
+    condition.setHasMonthlyTransactionLimit(true);
+    condition.setMonthlyTransactionLimit(3);
+    condition.setHasDailyWithdrawalTransactionLimit(false);
+    condition.setDailyWithdrawalTransactionLimit(0);
+    condition.setHasDailyDepositTransactionLimit(false);
+    condition.setDailyDepositTransactionLimit(0);
+    condition.setProductPerPersonLimit(1);
+    condition.setProductPerBusinessLimit(0);
+    
+    products.setCondition(condition);
+    
     List<Product> list = new ArrayList<>();
     list.add(products);
     Flux<Product> fluxproduct = Flux.fromIterable(list);
     
     Mockito.when(repository.findAll()).thenReturn(fluxproduct);
+    
     product.get().uri("/product").accept(MediaType.APPLICATION_JSON)
     .exchange().expectStatus().isOk().expectHeader().contentType(MediaType.APPLICATION_JSON)
     .expectBodyList(Product.class).hasSize(1);
@@ -66,19 +76,128 @@ class ProductControllerTest {
         
   }
 
-//  @Test
-//  void testFindById() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  void testCreate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  @Test
-//  void testUpdate() {
-//    fail("Not yet implemented");
-//  }
+  @Test
+  void testFindById() {
+    
+    Product products = new Product();
+    
+    List<String> type = new ArrayList<>();    
+    type.add("PERSONAL");
+    
+    products.setId("1");
+    products.setProductName("AHORRO");
+    products.setProductType("PASIVO");
+    
+    Condition condition = new Condition();
+    
+    condition.setCustomerTypeTarget(type);
+    condition.setHasMaintenanceFee(false);
+    condition.setMaintenanceFee(0.0);
+    condition.setHasMonthlyTransactionLimit(true);
+    condition.setMonthlyTransactionLimit(3);
+    condition.setHasDailyWithdrawalTransactionLimit(false);
+    condition.setDailyWithdrawalTransactionLimit(0);
+    condition.setHasDailyDepositTransactionLimit(false);
+    condition.setDailyDepositTransactionLimit(0);
+    condition.setProductPerPersonLimit(1);
+    condition.setProductPerBusinessLimit(0);
+    
+    products.setCondition(condition);
+    
+    Mockito.when(repository.findById("1")).thenReturn(Mono.just(products));
+    product.get().uri("/product/1")
+    .accept(MediaType.APPLICATION_JSON).exchange()
+    .expectStatus().isOk()
+    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+    .expectBody()
+    .jsonPath("$.id").isNotEmpty()
+    .jsonPath("$.productName").isEqualTo("AHORRO")
+    .jsonPath("$.productType").isEqualTo("PASIVO");
+  }
+
+  @Test
+  void testCreate() {
+    
+    Product products = new Product();
+    
+    List<String> type = new ArrayList<>();    
+    type.add("PERSONAL");
+    
+    products.setId("2");
+    products.setProductName("CREDITO PERSONAL");
+    products.setProductType("ACTIVO");
+    
+    Condition condition = new Condition();
+    
+    condition.setCustomerTypeTarget(type);
+    condition.setHasMaintenanceFee(false);
+    condition.setMaintenanceFee(0.0);
+    condition.setHasMonthlyTransactionLimit(false);
+    condition.setMonthlyTransactionLimit(0);
+    condition.setHasDailyWithdrawalTransactionLimit(false);
+    condition.setDailyWithdrawalTransactionLimit(0);
+    condition.setHasDailyDepositTransactionLimit(false);
+    condition.setDailyDepositTransactionLimit(0);
+    condition.setProductPerPersonLimit(1);
+    condition.setProductPerBusinessLimit(0);
+    
+    products.setCondition(condition);
+    
+    Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(Mono.just(products));
+    product.post().uri("/product")
+    .contentType(MediaType.APPLICATION_JSON)
+    .accept(MediaType.APPLICATION_JSON)
+    .body(Mono.just(products), Product.class)
+    .exchange()
+    .expectStatus().isCreated()
+    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+    .expectBody()
+    .jsonPath("$.id").isNotEmpty()
+    .jsonPath("$.productName").isEqualTo("CREDITO PERSONAL")
+    .jsonPath("$.productType").isEqualTo("ACTIVO");
+    
+  }
+
+  @Test
+  void testUpdate() {
+    
+    Product products = new Product();
+    
+    List<String> type = new ArrayList<>();    
+    type.add("PERSONAL");
+    
+    products.setId("2");
+    products.setProductName("PLAZO FIJO");
+    products.setProductType("PASIVO");
+    
+    Condition condition = new Condition();
+    
+    condition.setCustomerTypeTarget(type);
+    condition.setHasMaintenanceFee(false);
+    condition.setMaintenanceFee(0.0);
+    condition.setHasMonthlyTransactionLimit(false);
+    condition.setMonthlyTransactionLimit(0);
+    condition.setHasDailyWithdrawalTransactionLimit(false);
+    condition.setDailyWithdrawalTransactionLimit(0);
+    condition.setHasDailyDepositTransactionLimit(false);
+    condition.setDailyDepositTransactionLimit(0);
+    condition.setProductPerPersonLimit(1);
+    condition.setProductPerBusinessLimit(0);
+    
+    products.setCondition(condition);
+    
+    Mockito.when(repository.findById("2")).thenReturn(Mono.just(products));
+    Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(Mono.just(products));
+    product.put().uri("/product/2")
+    .accept(MediaType.APPLICATION_JSON)
+    .body(Mono.just(products), Product.class)
+    .exchange()
+    .expectStatus().isOk()
+    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+    .expectBody()
+    .jsonPath("$.id").isNotEmpty()
+    .jsonPath("$.productName").isEqualTo("PLAZO FIJO")
+    .jsonPath("$.productType").isEqualTo("PASIVO");
+  }
 
 }
